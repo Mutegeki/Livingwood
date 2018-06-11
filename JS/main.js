5161300f7b2842cb2628b1d3ebaadc9812442d6f
Mutegeki <braynmute@gmail.com>
2018-06-11 04:40:56 -0400

    Added a nav background in main.css file

--- JS/main.js
+++ JS/main.js
@@ -16,6 +16,6 @@ function showSlides() {
         dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+    //dots[slideIndex - 1].className += " active";
     setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
